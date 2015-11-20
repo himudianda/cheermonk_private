651e86a4c101ee71285c56f73d20401a55210b14
Harshit Imudianda <harshit.imudianda@gmail.com>
2015-11-20 01:39:46 -0800

    New script for theme transformations being created

---
+++ scripts/lib/dir_tree.py
@@ -0,0 +1,50 @@
+import os
+import shutil
+
+
+def create_dir_tree(source_folder, destination_folder, dirs_to_ignore):
+    '''
+        Copy the entire directory tree recursively
+    '''
+    for root, dirs, files in os.walk(source_folder):
+        sub_path = root.replace(source_folder, "")
+        dest_dir_rel_path = ''.join([destination_folder, sub_path])
+
+        if any(item in sub_path and sub_path.startswith(item) for item in dirs_to_ignore):
+            continue
+
+        if not os.path.exists(dest_dir_rel_path):
+            os.mkdir(dest_dir_rel_path)
+
+
+def copy_dir_tree(source_folder, destination_folder, dirs_to_ignore, files_to_ignore):
+    '''
+        Copy the entire directory tree recursively
+    '''
+    for root, dirs, files in os.walk(source_folder):
+        sub_path = root.replace(source_folder, "")
+        dest_dir_rel_path = ''.join([destination_folder, sub_path])
+
+        if any(item in sub_path and sub_path.startswith(item) for item in dirs_to_ignore):
+            continue
+
+        for filename in files:
+            src_file_path = os.path.join(root, filename)
+            dst_file_path = os.path.join(dest_dir_rel_path, filename)
+
+            if any(item in dst_file_path for item in files_to_ignore):
+                continue
+
+            if os.path.exists(dst_file_path):
+                if os.stat(src_file_path).st_mtime > os.stat(dst_file_path).st_mtime:
+                    shutil.copy2(src_file_path, dst_file_path)
+            else:
+                shutil.copy2(src_file_path, dst_file_path)
+
+
+def delete_dir_tree(directoy):
+    '''
+        Delete entire directoy tree
+    '''
+    if os.path.exists(directoy):
+        shutil.rmtree(directoy)
---
+++ scripts/new_transforms.py
@@ -0,0 +1,87 @@
+import os
+from lib.dir_tree import create_dir_tree, copy_dir_tree, delete_dir_tree
+
+
+cur_dir = os.getcwd()
+parent_dir = os.path.dirname(cur_dir)
+themes_dir = os.path.join(parent_dir, "_full_backups/themes")
+tmp_dest_dir = "/tmp"
+
+METRONIC = {
+    'name': "metronic",
+    'dirname': "Metronic_v4.5.2",
+    'original_dirname': os.path.join(themes_dir, "Metronic_v4.5.2"),
+    'temp_dirname': os.path.join(tmp_dest_dir, "Metronic_v4.5.2"),
+    'words_to_replace': {
+        'metronic': 'cheermonk',
+        'Metronic': 'Cheermonk',
+        'themeforest.net': 'cheermonk.com',
+        'themeforest': 'cheermonk',
+        'keenthemes.com': 'cheermonk.com',
+        'keenthemes': 'cheermonk',
+        'KeenThemes': 'Cheermonk'
+    },
+    'dirs_to_ignore': [
+        '/_documentation', '/_resources', '/_start', '/theme_rtl',
+        '/theme/admin_1', '/theme/admin_1_angularjs', '/theme/admin_1_material_design', '/theme/admin_1_rounded',
+        '/theme/admin_2_angularjs', '/theme/admin_2_material_design', '/theme/admin_2_rounded',
+        '/theme/admin_3', '/theme/admin_3_angularjs', '/theme/admin_3_material_design', '/theme/admin_3_rounded',
+        '/theme/admin_4', '/theme/admin_4_angularjs', '/theme/admin_4_material_design', '/theme/admin_4_rounded',
+        '/theme/admin_5', '/theme/admin_5_material_design', '/theme/admin_5_rounded',
+        '/theme/admin_6', '/theme/admin_6_material_design', '/theme/admin_6_rounded',
+        '/theme/admin_7', '/theme/admin_7_material_design', '/theme/admin_7_rounded',
+        '/theme/sass', '/theme/demo'
+    ],
+    'files_to_ignore': [
+        '/readme.txt', '/start.html', '/theme/gulpfile.js', '/theme/package.json'
+    ]
+}
+
+CANVAS = {
+    'name': "canvas",
+    'dirname': "Canvas_v3.0.1",
+    'original_dirname': os.path.join(themes_dir, "Canvas_v3.0.1"),
+    'temp_dirname': os.path.join(tmp_dest_dir, "Canvas_v3.0.1"),
+    'words_to_replace': {
+        'canvas': 'cheermonk',
+        'Canvas': 'Cheermonk',
+        'themeforest.net': 'cheermonk.com',
+        'themeforest': 'cheermonk',
+        'semicolonweb.com': 'cheermonk.com',
+        'semicolonweb': 'cheermonk',
+        'SemiColonWeb': 'Cheermonk'
+    },
+    'dirs_to_ignore': ['/Documentation', '/HTML-RTL'],
+    'files_to_ignore': []
+}
+
+THEMES = [METRONIC, CANVAS]
+
+
+def delete_old_tmp_copy(theme):
+    '''
+        Deletes previous copy of theme from /tmp directoy
+    '''
+    delete_dir_tree(theme['temp_dirname'])
+
+
+def copy_theme_to_tmp(theme):
+    '''
+        Create a copy of the theme in /tmp directoy
+    '''
+    source_folder = theme['original_dirname']
+    dst_folder = theme['temp_dirname']
+
+    # Create the destination directory structure first
+    create_dir_tree(source_folder, dst_folder, theme['dirs_to_ignore'])
+    copy_dir_tree(source_folder, dst_folder, theme['dirs_to_ignore'], theme['files_to_ignore'])
+
+
+def main():
+    for theme in THEMES:
+        delete_old_tmp_copy(theme)
+        copy_theme_to_tmp(theme)
+
+
+if __name__ == "__main__":
+    main()
