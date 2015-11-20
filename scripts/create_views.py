1d293a16bf3e6bb95abcafc0a12671f7858e3f2f
Harshit Imudianda <harshit.imudianda@gmail.com>
2015-11-19 17:10:28 -0800

    Better code

--- scripts/create_views.py
+++ scripts/create_views.py
@@ -7,29 +7,22 @@ templates = ["sample_dashboard", "sample_frontend"]
 views_template = Template('''
 @sample_dashboard.route('/$name')
 def $name():
-    return render_template('$template/$path')
+    return render_template('$template$path')
 ''')
 
 
 def list_files(template):
     dirname = os.path.join(templates_dir, template)
     for root, dirs, files in os.walk(dirname):
-        print root, dirname
-
         for file in files:
-            name = root.replace(dirname, '')
+            rel_dir = root.replace(dirname, '')
 
             file_no_ext, ext = os.path.splitext(file)
-            name = '_'.join([name, file_no_ext])
-
-            name = name.replace('/', '_')
-            name = name.replace('-', '_')
-
+            name = '_'.join([rel_dir, file_no_ext])
+            name = name.replace('/', '_').replace('-', '_')
             name = name[1:]
 
-            new_name = root.replace(dirname, '')
-            new_name = '/'.join([new_name, file])
-            path = ''.join([template, new_name])
+            path = '/'.join([rel_dir, file])
 
             print views_template.substitute(name=name, path=path, template=template)
 
